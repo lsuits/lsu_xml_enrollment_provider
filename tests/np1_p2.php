a35e4446e111f2884aa02001bff7843f19d5abb5
Jason Peak <jpeak5@lsu.edu>
2014-02-20 10:45:05 -0600

    finishes test for  np1->p2

--- tests/np1_p2.php
+++ tests/np1_p2.php
@@ -6,7 +6,7 @@ class cron_enrol_initialNonPrimarySwappedOutForNewPrimary extends local_lsu_test
 
     static $local_datadir = 'np1_p2/';
 
-    public function test_step1_initialEnrollment(){
+    public function test_step1(){
         global $DB;
         
         // set test data files as input to the process
@@ -14,18 +14,19 @@ class cron_enrol_initialNonPrimarySwappedOutForNewPrimary extends local_lsu_test
         
         //run cron against initial dataset
         $this->ues->cron();
-
         $this->assertEmpty($this->ues->errors, sprintf("UES finished with errors"));
 
-        // make assertions about enrollment as it should appear after step 1
-        
         // there should be one course
         $this->assertEquals(1, count($DB->get_records_sql(self::$coursesSql)));
-        $this->assertTrue((bool)$this->getCourseIfExists('2014 Spring TST1 1350 for instructor one'));
-        $this->assertTrue($this->userHasRoleInCourse('inst1', 'teacher', '2014 Spring TST1 1350 for instructor one'));
-        
-        $students = $this->usersWithRoleInCourse('student', '2014 Spring TST1 1350 for instructor one');
-        $this->assertEquals(29, count($students));
+        $inst1Course = $this->getCourseIfExists('2014 Spring TST1 1350 for instructor one');
+        $this->assertTrue((bool)$inst1Course);
+
+        // inst1 as non-primary, no other teacher enrollments, 29 students, visible
+        $this->assertTrue($this->userHasRoleInCourse('inst1', 'teacher', $inst1Course->fullname));
+        $this->assertEquals(1, count($this->usersWithRoleInCourse('teacher', $inst1Course->fullname)));
+        $this->assertEquals(0, count($this->usersWithRoleInCourse('editingteacher', $inst1Course->fullname)));
+        $this->assertEquals(29, count($this->usersWithRoleInCourse('student', $inst1Course->fullname)));
+        $this->assertEquals(1, $inst1Course->visible);
         
         $this->endOfStep();
     }
@@ -33,73 +34,42 @@ class cron_enrol_initialNonPrimarySwappedOutForNewPrimary extends local_lsu_test
     public function test_step2_swapOutNonPrimaryCreatesNewCourse(){
         global $DB;
         
-        // set test data files as input to the process
+        // step 1
         $this->set_datasource_for_stage(1);
-
-        //run cron against initial dataset
         $this->ues->cron();
-        
-        // ensure no errors
         $this->assertEmpty($this->ues->errors, sprintf("UES finished with errors"));
-
-        // ensure there are 30 user enrollments
-        $this->assertEquals(30, count($DB->get_records('user_enrolments')));
-        
         $this->endOfStep();
         
         // ----------------- STAGE 2 ---------------------- //
         
-        // set test data files as input to the process
         $this->set_datasource_for_stage(2);
-        
-        // ensure there are no invalid groups
-        $provider = $this->ues->provider();
-        $this->assertEquals(0, count($provider->findOrphanedGroups()));
-        
-        //run cron against step 2 dataset
         $this->ues->cron();
-        
-        // ensure no errors
         $this->assertEmpty($this->ues->errors, sprintf("UES finished with errors"));
 
         // inst2 course should have been created
         $inst2Course = $this->getCourseIfExists('2014 Spring TST1 1350 for instructor two');
-        
         $this->assertTrue((bool)$inst2Course);
-        
-        // ensure there are no invalid groups
-        $this->assertEquals(0, count($provider->findOrphanedGroups()));
-        
-        // ensure there are 30 user enrollments
-        $this->assertEquals(31, count($DB->get_records('user_enrolments')));
 
         // there should be two courses
         $this->assertEquals(2, count($DB->get_records_sql(self::$coursesSql)));
 
         
-        // inst2 should be the teacher
-        $this->assertTrue($this->userHasRoleInCourse('inst2', 'editingteacher', '2014 Spring TST1 1350 for instructor two'));
-        
-        // course should be visible
+        // inst2 should be the editing teacher, course should be visible
+        $this->assertTrue($this->userHasRoleInCourse('inst2', 'editingteacher', $inst2Course->fullname));
+        $this->assertEquals(29, count($this->usersWithRoleInCourse('student', $inst2Course->fullname)));
+        $this->assertEquals(1, count($this->usersWithRoleInCourse('editingteacher', $inst2Course->fullname)));
+        $this->assertEquals(0, count($this->usersWithRoleInCourse('teacher', $inst2Course->fullname)));
         $this->assertEquals(1, $inst2Course->visible);
-        
-        // student enrollment count
-        $this->assertEquals(29, count($this->usersWithRoleInCourse('student', '2014 Spring TST1 1350 for instructor two')));
-        
-        // ------------------------------------- //
 
-        // inst1 course should still exist
+        // inst1 course should still exist, empty of students, with inst1 as the only teacher role, coursse should be invisible
         $inst1Course = $this->getCourseIfExists('2014 Spring TST1 1350 for instructor one');
-        $this->assertTrue((bool)$inst1Course);
-
-        // inst1 should be the non-primary instructor, as before
-        $this->assertTrue($this->userHasRoleInCourse('inst1', 'teacher', '2014 Spring TST1 1350 for instructor one'));
-
-        // the course should be invisible
-        $this->assertEquals(0, $inst1Course->visible);
         
-        // student enroillment should be 0
-        $this->assertEquals(0,  count($this->usersWithRoleInCourse('student', '2014 Spring TST1 1350 for instructor one')));
+        $this->assertTrue((bool)$inst1Course);
+        $this->assertTrue($this->userHasRoleInCourse('inst1', 'teacher', $inst1Course->fullname));
+        $this->assertEquals(0,  count($this->usersWithRoleInCourse('student', $inst1Course->fullname)));
+        $this->assertEquals(0,  count($this->usersWithRoleInCourse('editingteacher', $inst1Course->fullname)));
+        $this->assertEquals(1,  count($this->usersWithRoleInCourse('teacher', $inst1Course->fullname)));
+        $this->assertEquals(0, $inst1Course->visible);
         
         $this->endOfStep();
         
